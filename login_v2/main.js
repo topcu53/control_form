7fb7261db26d6c1b0b571bf7ab933094228df576
topcu53 <btopcu53@icloud.com>
2023-10-31 15:59:19 +0300

    update

--- login_v2/main.js
+++ login_v2/main.js
@@ -2,16 +2,17 @@ var myname = document.getElementById("name");
 var lastname = document.getElementById("lastname");
 var username = document.getElementById("username");
 
-
-
 myname.addEventListener("keyup", run); // tuştan elini kaldırdığında
 lastname.addEventListener("keyup", run);
 
 function birlesim() {
-
-    username.value = myname.value.toLowerCase() + "." + lastname.value.toLowerCase();
+  username.value =
+    myname.value.toLowerCase() +
+    "." +
+    lastname.value.toLowerCase() +
+    "@ozdilek.com.tr";
 }
 function run(e) {
-    e.preventDefault();
-    birlesim();
+  e.preventDefault();
+  birlesim();
 }
