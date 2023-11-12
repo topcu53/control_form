0c1c85001ebc5c04b23271883bb9fd985ea9ce8a
AdminKostak <muhammedkostak@gmail.com>
2023-11-12 16:56:29 +0300

    update sunday

--- ControlForm/control.js
+++ ControlForm/control.js
@@ -11,6 +11,7 @@ var fname = document.getElementById('fname'),
     tabBooleans = [false, false, false, false, false, false, false, false, false];
 
     
+   
 
     window.onload = function () {
         var script = document.createElement("script");
